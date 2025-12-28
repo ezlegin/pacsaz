f2e9ac44aa6687604b837d067c070f5c809154fb
Alireza Ezlegini <alireza.ezlegini@yahoo.com>
2025-12-28 12:40:03 +0330

    Enhance: make the svg preview work correct and more consize

--- apps/web/app/test/page.tsx
+++ apps/web/app/test/page.tsx
@@ -3,77 +3,56 @@ import { onDevelepe } from "@/lib/dielines/core/consts";
 import { Button } from "@workspace/ui/components/button";
 import { Card } from "@workspace/ui/components/card";
 import { Separator } from "@workspace/ui/components/separator";
-import { Spinner } from "@workspace/ui/components/spinner";
 import { ZoomIn, ZoomOut } from "lucide-react";
-import { useLayoutEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import {
   ReactZoomPanPinchRef,
   TransformComponent,
   TransformWrapper,
 } from "react-zoom-pan-pinch";
 
-export default function SvgPreview({
-  svg,
-  isRendering,
-}: {
-  svg: string;
-  isRendering: boolean;
-}) {
+export default function Page() {
   const tranformRef = useRef<ReactZoomPanPinchRef | null>(null);
   const contentRef = useRef<HTMLDivElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
 
-  useLayoutEffect(() => {
+  const height = 2;
+
+  useEffect(() => {
     if (!contentRef.current || !wrapperRef.current) return;
 
-    const measureAndCenter = () => {
-      const contentHeight = contentRef.current!.clientHeight || 1;
-      const wrapperHeight = wrapperRef.current!.clientHeight || 1;
-      const scaleY = wrapperHeight / contentHeight;
+    const contentHeight = contentRef.current.clientHeight;
+    const wrapperHeight = wrapperRef.current.clientHeight;
+    const scaleY = wrapperHeight / contentHeight;
 
-      const contentWidth = contentRef.current!.clientWidth || 1;
-      const wrapperWidth = wrapperRef.current!.clientWidth || 1;
-      const scaleX = wrapperWidth / contentWidth;
+    const contentWidth = contentRef.current.clientWidth;
+    const wrapperWidth = wrapperRef.current.clientWidth;
+    const scaleX = wrapperWidth / contentWidth;
 
-      const scale = Math.min(scaleX, scaleY);
-      if (isFinite(scale) && scale > 0) {
-        tranformRef.current?.centerView(scale);
-      }
-    };
+    const chosenScale = Math.min(scaleX, scaleY);
 
-    const raf = requestAnimationFrame(measureAndCenter);
+    const scalePadding =
+      scaleY <= scaleX
+        ? 0.2 * scaleY // Y Padding
+        : 0.4 * scaleX; // X Padding
 
-    const imgs = Array.from(contentRef.current.querySelectorAll("img"));
-    const onImgLoad = () => requestAnimationFrame(measureAndCenter);
-    imgs.forEach((i) => i.addEventListener("load", onImgLoad));
+    const scale = chosenScale - scalePadding;
 
-    return () => {
-      cancelAnimationFrame(raf);
-      imgs.forEach((i) => i.removeEventListener("load", onImgLoad));
-    };
-  }, [svg]);
+    tranformRef.current?.centerView(scale);
+  }, [height]);
 
   return (
     <div
       ref={wrapperRef}
       dir="ltr"
-      className="h-full w-screen flex items-center justify-center"
+      className="h-screen w-screen flex items-center justify-center"
     >
       <TransformWrapper
-        key={svg}
         ref={tranformRef}
         centerOnInit
         limitToBounds={false}
         minScale={0.5}
         maxScale={onDevelepe ? 10 : 3}
-        panning={{ disabled: isRendering }}
-        wheel={{ disabled: isRendering }}
-        velocityAnimation={{ disabled: true }}
-        alignmentAnimation={{ disabled: true }}
-        zoomAnimation={{ disabled: true }}
-        doubleClick={{ disabled: true }}
-        smooth
-        centerZoomedOut
       >
         {({ zoomIn, zoomOut, resetTransform }) => (
           <>
@@ -81,16 +60,16 @@ export default function SvgPreview({
               wrapperStyle={{
                 width: "100%",
                 height: "100%",
+                background: "red",
+                overflow: "hidden",
               }}
+              contentStyle={{ background: "blue" }}
             >
-              <div className="relative" ref={contentRef}>
-                {isRendering && (
-                  <Spinner className="scale-200 text-primary absolute top-1/2 translate-y-1/2 left-1/2 translate-x-1/2" />
-                )}
-                <div
-                  dangerouslySetInnerHTML={{ __html: svg }}
-                  className={isRendering ? "opacity-50" : "opacity-100"}
-                />
+              <div
+                ref={contentRef}
+                className={`bg-amber-300 rounded-full w-[500px] text-center p-10`}
+              >
+                Content
               </div>
             </TransformComponent>
 
